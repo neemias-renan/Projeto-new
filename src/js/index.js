b2b6bec7f350a3cf97b68f9f185785ee677114ba
Neemias Renan <neemiasrenan2015@gmail.com>
2022-01-08 16:48:45 -0300

    adição de novos elementos

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,15 @@ function closemenu(){
     document.getElementById('button_menu-close').style.display = 'none';
     document.getElementById('button_menu-open').style.display = 'block';
 }
+
+function openstreaming(streaming){
+    document.getElementById('container-netflix').style.display = 'none'
+    document.getElementById('container-primevideo').style.display = 'none';
+    document.getElementById('container-disneyplus').style.display = 'none';
+    document.getElementById('container-hbomax').style.display = 'none';
+    document.getElementById('container-globoplay').style.display = 'none';
+    document.getElementById('container-paramount').style.display = 'none';
+    document.getElementById('container-telecineplay').style.display = 'none';
+
+    document.getElementById(streaming).style.display = 'block';
+}
